41940d1a5d822cfc1810191dbdf91c84cc90d4e0
John Harding <john.a.harding@ericsson.com>
2014-10-16 09:33:38 +0100

    Remove all whitespace in customer ID text field, not just trim()

--- src/suncertify/presentation/MainWindow.java
+++ src/suncertify/presentation/MainWindow.java
@@ -40,67 +40,67 @@ import suncertify.db.DBException;
 import suncertify.db.RecordNotFoundException;
 import suncertify.db.SecurityException;
 import suncertify.network.NetworkException;
- 
+
 
 public class MainWindow extends JFrame implements Observer {
-	
+
 	/**
      * The SUID
      */
     private static final long serialVersionUID = 1771L;
-    
+
     /**
      * Mask format that only allows digits in the customer ID field.
      */
     private static final String EIGHT_DIGITS_MASK = "########";
-    
+
     /**
      * <code>BusinessService</code> is the service class
      * that provides functionality to search and book records.
      */
     private BusinessService service;
-    
+
     /**
      * The last search performed.
      */
     private SearchCriteria lastSearch = null;
-    
+
     /**
      * Table model for <code>JTable</code>.
      */
 	private RoomTableModel tableModel = new RoomTableModel();
-	
+
 	/**
 	 * <code>JTable</code> used to display records.
 	 */
 	private JTable table;
-	
+
 	/**
 	 * Text fields for hotel name search.
 	 */
 	private JTextField nameTextField;
-	
+
 	/**
 	 * Text field for hotel location search.
 	 */
 	private JTextField locationTextField;
-	
+
 	/**
 	 * Text field for inputting a customer ID
 	 * in order to book a room.
 	 */
 	private JTextField customerIdTextField;
-	
+
 	/**
 	 * Search button.
 	 */
 	private JButton searchButton;
-	
+
 	/**
 	 * Book button.
 	 */
 	private JButton bookButton;
-	
+
 	/**
 	 * Class constructor.
 	 * @param businessService the <code>BusinessService</code>
@@ -113,7 +113,7 @@ public class MainWindow extends JFrame implements Observer {
         setUpGUI();
         refreshTable();
     }
-    
+
     /**
      * Refreshes the table model.
      */
@@ -122,7 +122,7 @@ public class MainWindow extends JFrame implements Observer {
         // TODO Auto-generated method stub
         tableModel.fireTableDataChanged();
     }
-    
+
     /**
      * Sets up the GUI by instantiating and positioning
      * various GUI components.
@@ -140,18 +140,18 @@ public class MainWindow extends JFrame implements Observer {
         createStripedTable();
         table.setSelectionBackground(Color.decode("#8AA37B"));
         table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        
+
         JPanel mainPanel = new JPanel(new GridBagLayout());
         mainPanel.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED));
-        
+
         GridBagConstraints c;
-        
+
         c = new GridBagConstraints();
         c.gridx = 0;
         c.gridy = 0;
         c.fill = GridBagConstraints.VERTICAL;
         mainPanel.add(createSideBarPanel(), c);
-        
+
         c = new GridBagConstraints();
         c.gridx = 1;
         c.gridy = 0;
@@ -159,30 +159,31 @@ public class MainWindow extends JFrame implements Observer {
         c.weighty = 1.0;
         c.fill = GridBagConstraints.BOTH;
         mainPanel.add(createTablePanel(), c);
-        
+
         c = new GridBagConstraints();
         c.gridx = 1;
         c.ipadx = 4;
         c.ipady = 10;
         c.anchor = GridBagConstraints.EAST;
         mainPanel.add(createBookPanel(), c);
-        
+
         add(mainPanel);
         setMinimumSize(new Dimension(750, 575));
         setLocationRelativeTo(null);
         pack();
         setVisible(true);
     }
-    
+
     /**
      * Creates a striped JTable.
      */
     private void createStripedTable() {
-        
+
         table = new JTable(tableModel) {
-            
+
             private static final long serialVersionUID = 2442L;
 
+            @Override
             public Component prepareRenderer(TableCellRenderer renderer, int row, int column) {
                 Component comp = super.prepareRenderer(renderer, row, column);
                 if (!isRowSelected(row)) {
@@ -195,16 +196,16 @@ public class MainWindow extends JFrame implements Observer {
             }
         };
     }
-    
+
     /**
      * Creates the side bar JPanel that contains the search controls.
      */
     private JPanel createSideBarPanel() {
     	JPanel panel = new JPanel(new GridBagLayout());
     	panel.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED));
-    	
+
     	GridBagConstraints c;
-        
+
         JLabel nameLabel = new JLabel("Name");
         c = new GridBagConstraints();
         c.gridx = 0;
@@ -213,26 +214,26 @@ public class MainWindow extends JFrame implements Observer {
         c.ipady = 4;
         c.insets = new Insets(100, 5, 5, 5);
         panel.add(nameLabel, c);
-        
+
         nameTextField = new JTextField(15);
         c = new GridBagConstraints();
         c.gridx = 1;
         c.gridy = 1;
         c.insets = new Insets(100, 5, 5, 5);
         panel.add(nameTextField, c);
-        
+
         JLabel locationLabel = new JLabel("Location");
         c = new GridBagConstraints();
         c.gridx = 0;
         c.gridy = 2;
         panel.add(locationLabel, c);
-        
+
         locationTextField = new JTextField(15);
         c = new GridBagConstraints();
         c.gridx = 1;
         c.gridy = 2;
         panel.add(locationTextField, c);
-        
+
         searchButton = new JButton("Search");
         searchButton.addActionListener(new SearchButtonListener());
         c = new GridBagConstraints();
@@ -242,10 +243,10 @@ public class MainWindow extends JFrame implements Observer {
         c.insets = new Insets(10, 5, 5, 5);
         c.anchor = GridBagConstraints.NORTHEAST;
         panel.add(searchButton, c);
-        
+
         return panel;
     }
-    
+
     /**
      * Creates the JPanel that contains the table.
      */
@@ -254,26 +255,26 @@ public class MainWindow extends JFrame implements Observer {
     	panel.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED));
         table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
         panel.add(new JScrollPane(table));
-        
+
         return panel;
     }
-    
+
     /**
      * Creates the JPanel that contains the booking controls.
      */
     private JPanel createBookPanel() {
-    	
+
     	JPanel panel = new JPanel(new GridBagLayout());
-    	
+
     	GridBagConstraints c;
-    	
+
     	JLabel customerIdLabel = new JLabel("Customer ID:");
     	c = new GridBagConstraints();
     	c.gridx = 0;
     	c.gridy = 0;
     	c.insets = new Insets(10, 0, 0, 15);
         panel.add(customerIdLabel, c);
-        
+
         MaskFormatter eightDigits = null;
 		try {
 			eightDigits = new MaskFormatter(EIGHT_DIGITS_MASK);
@@ -283,7 +284,7 @@ public class MainWindow extends JFrame implements Observer {
 			 * While catch blocks that swallow exceptions are a bad idea
 			 * in this case I feel that it is acceptable given that the
 			 * mask to be used is a pre-defined hard coded constant.  As
-			 * a result ParseException should never be thrown. 
+			 * a result ParseException should never be thrown.
 			 */
 		}
 		customerIdTextField.setColumns(10);
@@ -292,7 +293,7 @@ public class MainWindow extends JFrame implements Observer {
         c.gridy = 0;
         c.insets = new Insets(10, 0, 0, 25);
         panel.add(customerIdTextField, c);
-        
+
         bookButton = new JButton("Book");
         bookButton.setEnabled(false);
         customerIdTextField.addKeyListener(new ButtonEnabler(customerIdTextField, bookButton, 8));
@@ -306,11 +307,11 @@ public class MainWindow extends JFrame implements Observer {
 
         return panel;
     }
-    
+
     /**
      * Refreshes the table data by performing the last search, if there was
      * a previous search.  If the lastSearch variable is null, a search for all
-     * records is performed. This functionality was introduced to update the 
+     * records is performed. This functionality was introduced to update the
      * data displayed to the user if he / she tries to book a record that has
      * been booked by another remote client in the time between performing
      */
@@ -333,20 +334,20 @@ public class MainWindow extends JFrame implements Observer {
             Dialogs.showErrorDialog(this, "Cannot show the latest bookings", "Network error");
         }
     }
-    
+
     /**
      * Adds a listener to the window exit button
      */
     private void addExitListener() {
         addWindowListener(new WindowAdapter() {
-            
+
             @Override
             public void windowClosing(WindowEvent e) {
                 service.cleanUp();
             }
         });
     }
-    
+
     /**
      * ActionListener for quit menu item functionality.
      */
@@ -358,20 +359,20 @@ public class MainWindow extends JFrame implements Observer {
 			System.exit(0);
 		}
     }
-    
+
     /**
      * KeyListener that enables the book button if a row is
      * selected and an 8 digit customer ID is entered into
      * the appropriate field.
      */
     private class ButtonEnabler implements KeyListener {
-    	
+
     	JTextField textField;
-    	
+
     	JButton button;
-    	
+
     	int inputLength;
-    	
+
     	ButtonEnabler(JTextField textField, JButton button, int inputLength) {
     		this.textField = textField;
     		this.button = button;
@@ -380,10 +381,10 @@ public class MainWindow extends JFrame implements Observer {
 
 		@Override
 		public void keyReleased(KeyEvent arg0) {
-			String input = this.textField.getText().trim();
+			String input = this.textField.getText().replaceAll("\\s","");
 			boolean inputIsLongEnough = input.length() >= this.inputLength;
 			boolean aRowIsSelected = table.getSelectedRow() != -1;
-			
+
 			if (inputIsLongEnough && aRowIsSelected) {
 				this.button.setEnabled(true);
 			}
@@ -391,37 +392,37 @@ public class MainWindow extends JFrame implements Observer {
 				this.button.setEnabled(false);
 			}
 		}
-		
+
 		@Override
 		public void keyPressed(KeyEvent arg0) {}
 
 		@Override
 		public void keyTyped(KeyEvent arg0) {}
-    	
+
     }
-    
+
     /**
      * ActionListener for search button functionality.
      */
     private class SearchButtonListener implements ActionListener {
-        
+
         MainWindow parent = MainWindow.this;
-        
+
 		@Override
         public void actionPerformed(ActionEvent e) {
             String name = nameTextField.getText().trim();
             String location = locationTextField.getText().trim();
             Map<Integer, Room> matches = new LinkedHashMap<Integer, Room>();
             SearchCriteria criteria = new SearchCriteria();
-            
+
             if (name.equals("") && location.equals("")) {
                 criteria.matchAllRecords();
             }
             else {
-                criteria.matchName(name)    
+                criteria.matchName(name)
                         .matchLocation(location);
             }
-            
+
             try {
                 matches = service.searchRooms(criteria);
             } catch (DBException ex) {
@@ -433,25 +434,25 @@ public class MainWindow extends JFrame implements Observer {
             tableModel.setRoomMap(matches);
         }
     }
-    
+
     /**
-     * ActionListener for book button functionality. 
+     * ActionListener for book button functionality.
      */
     private class BookButtonListener implements ActionListener {
-    	
+
     	MainWindow parent = MainWindow.this;
-    	
+
 		@Override
 		public void actionPerformed(ActionEvent e) {
 		    int selectedRowIndex = table.getSelectedRow();
 	        String customerId = customerIdTextField.getText();
 		    Room room = tableModel.getRoom(selectedRowIndex);
 		    room.setOwner(customerId);
-		    
+
 		    try {
 		        service.bookRoom(room);
             } catch (RecordNotFoundException ex) {
-                Dialogs.showErrorDialog(parent, "Sorry, this room is no longer available", 
+                Dialogs.showErrorDialog(parent, "Sorry, this room is no longer available",
                             "Room already booked");
             } catch (SecurityException ex) {
                 Dialogs.showErrorDialog(parent, "Could not complete booking", "Error");
@@ -462,10 +463,10 @@ public class MainWindow extends JFrame implements Observer {
             } finally {
                 refreshTable();
             }
-		    
+
 		    customerIdTextField.setText("");
 		    bookButton.setEnabled(false);
 		}
     }
-	
+
 }
