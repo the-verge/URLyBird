d94a7030eab29608d1d10dd55dfc724beb6cfc91
the-verge <username@users.noreply.github.com>
2014-10-25 17:55:54 +0100

    Add suncertify.presentation Javadocs #16
    Added BusinessService Javadocs

--- src/suncertify/presentation/BusinessService.java
+++ src/suncertify/presentation/BusinessService.java
@@ -1,7 +1,9 @@
 package suncertify.presentation;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Observer;
 
@@ -13,29 +15,67 @@ import suncertify.db.SecurityException;
 import suncertify.network.NetworkException;
 
 /**
+ * The <code>BusinessService</code> class is a utility
+ * class used by clients.  It accesses the database via 
+ * a <code>CloseableDB</code> instance, which may provide
+ * a local or remote database connection.
  * 
  * @author john
  *
  */
 public class BusinessService {
+    
+    /**
+     * Index of the owner field in a database record.
+     */
+    private static final int OWNER_INDEX = 6;
 	
-	private Observer observer;
+    /**
+     * An <code>Observer</code> instance.
+     */
+	private List<Observer> observers = new ArrayList<Observer>();
 	
+	/**
+	 * Provides database connectivity.
+	 */
 	private CloseableDB dataAccess;
 	
+	/**
+	 * Class constructor.
+	 * @param dataAccess a <code>CloseableDB</code>
+	 *        instance that provides database connectivity.
+	 */
 	public BusinessService(CloseableDB dataAccess) {
 		this.dataAccess = dataAccess;
 	}
 	
+	/**
+	 * Adds an <code>Observer</code> to the <code>observers</code> list.
+	 * @param observer an <code>Observer</code> instance.
+	 */
 	public void addObserver(Observer observer) {
-		this.observer = observer;
+		observers.add(observer);
 	}
 	
-	public void fireModelChangeEvent() {
-	    // INVESTIGATE
-		observer.update(null, null);
+	/**
+	 * Informs observers that data has changed.
+	 */
+	public void fireDataChangeEvent() {
+	    for (Observer o: observers) {
+	        o.update(null, null);
+	    }
 	}
 	
+	/**
+	 * Searches for data matching supplied criteria.
+	 * @param criteria <code>SearchCriteria</code> instance.
+	 * @return Map which is used to map match[i]
+	 *         to a database record number.
+	 * @throws DBException if a database failure occurs.
+	 * @throws NetworkException if the <code>CloseableDB</code>
+	 *         instance provides a remote database connection
+	 *         and a network error occurs.
+	 */
 	public Map<Integer, Room> searchRooms(SearchCriteria criteria) {
         
         String[] searchCriteria = criteria.getCriteria();
@@ -66,6 +106,19 @@ public class BusinessService {
         return roomMap;
     }   
 	
+	/**
+	 * Books a room.
+	 * @param room <code>Room</code> object encapsulating the data of the
+	 *        record to be updated.
+	 * @throws RecordNotFoundException if the requested room has already
+	 *         been booked.
+	 * @throws SecurityException if an attempt is made to book a 
+	 *         record with an invalid lock cookie.
+	 * @throws DBException if a database failure occurs.
+     * @throws NetworkException if the <code>CloseableDB</code>
+     *         instance provides a remote database connection
+     *         and a network error occurs.
+	 */
 	public void bookRoom(Room room) throws RecordNotFoundException, SecurityException {
 	    int recNo = room.getRecNo();
 	    String[] data = room.getData();
@@ -105,23 +158,42 @@ public class BusinessService {
                  * Nothing of use can be conveyed to the
                  * user if for some reason an Exception
                  * is thrown when unlocking a record. As
-                 * such the exception is handled here by
-                 * not doing anything.
+                 * such the exception is not propagated
+                 * further.
                  */
             }
 	    }
-		fireModelChangeEvent();
+		fireDataChangeEvent();
 	}
 	
+	/**
+	 * Checks if a given record is already booked.
+	 * @param recNo the record number to check.
+	 * @return boolean.
+	 * @throws RecordNotFoundException.
+	 */
 	private boolean alreadyBooked(int recNo) throws RecordNotFoundException {
 		String[] roomData = dataAccess.read(recNo);
-		String owner = roomData[6];
+		String owner = roomData[OWNER_INDEX];
 		if (owner.equals("")) {
 			return false;
 		}
 		return true;
 	}
 	
+	/**
+	 * Deletes a room.
+	 * @param room a <code>Room</code> object encapsulating the data of the
+	 *        record to delete.
+	 * @throws RecordNotFoundException if the record does not exist.
+	 *         (It could potentially have been deleted by another client).
+	 * @throws SecurityException if an attempt is made to delete a record 
+	 *         with an invalid lock cookie.
+     * @throws DBException if a database failure occurs.
+     * @throws NetworkException if the <code>CloseableDB</code>
+     *         instance provides a remote database connection
+     *         and a network error occurs.
+	 */
 	public void deleteRoom(Room room) throws RecordNotFoundException, SecurityException {
         int recNo = room.getRecNo();
         
@@ -161,13 +233,21 @@ public class BusinessService {
              */
         }
         
-        fireModelChangeEvent();
+        fireDataChangeEvent();
     }
 	
+	/**
+	 * Creates a room.
+	 * @param data the fields of the record to be created.
+	 * @throws DuplicateKeyException
+	 */
 	public void createRoom(String[] data) throws DuplicateKeyException {
         dataAccess.create(data);
     }
 	
+	/**
+	 * Closes local database connections.
+	 */
 	public void cleanUp() {
 	    try {
 	        boolean shouldClose = dataAccess.hasLocalDatabaseConnection();
